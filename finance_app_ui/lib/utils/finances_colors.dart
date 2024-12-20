9c23f5e9a5150d08c44e9f02cef724e338dfcf78
Samarth Garge <gargesamarth@gmail.com>
2024-12-20 17:51:17 +0530

    init

---
+++ finance_app_ui/lib/utils/finances_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FinancesColors {
+  static const white = Color.fromRGBO(255, 255, 255, 1);
+  static const purple = Color.fromRGBO(52, 35, 74, 1);
+  static const lightPurple = Color.fromRGBO(211, 184, 244, 1);
+  static const lightOrange = Color.fromRGBO(244, 198, 185, 1);
+  static const orange = Color.fromRGBO(234, 144, 120, 1);
+  static const green = Color.fromRGBO(84, 167, 129, 1);
+  static const lightYellow = Color.fromRGBO(243, 216, 157, 1);
+  static const lightPink = Color.fromRGBO(246, 215, 225, 1);
+  static const lightRed = Color.fromRGBO(240, 188, 201, 1);
+  static const lightBlue = Color.fromRGBO(99, 199, 244, 1);
+  static const veryLightYellow = Color.fromRGBO(249, 220, 161, 1);
+  static const lightBrown = Color.fromRGBO(214, 177, 133, 1);
+  static const lightGreen = Color.fromRGBO(86, 171, 135, 1);
+}
